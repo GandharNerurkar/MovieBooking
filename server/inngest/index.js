8d952c1eee200d7cca43a189800206c54521eed5
GandharNerurkar <gandhar.nerurkar@zapcg.com>
2025-07-09 16:09:43 +0530

    booking confirmation email

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,3 +1,4 @@
+import sendEmail from "../config/nodeMailer.js";
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
 import User from "../models/User.js";
@@ -73,6 +74,33 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction(
     }
 )
 
+// Send email when user book show
+const sendBookingConformationEmail = inngest.createFunction({
+    id: 'send-booking-conformation-email'},
+    {event: "app/show.booked"},
+    async ({event, step})=>{
+        const {bookingId} = event.data;
+
+        const booking = await Booking.findById(bookingId).populate({
+            path: 'show',
+            populate: {path: "movie", model: "Movie"}
+        }).populate('user')
+        await sendEmail({
+            to: booking.user.email,
+            subject: `Payment Confirmation: "${booking.show.movie.title}" booked!`,
+            body: `<div style="font-family:Arial, sans-serif; line-height: 1.5;">
+            <h2>Hi ${booking.user.name},</h2>
+            <p>Your booking for <strong style="color: #F84565;">"${booking.show.movie.title}"</strong> is confirmed.</p>
+            <p>
+            <strong>Date:</strong> ${new Date(booking.show.showDateTime).toLocaleDateString('en-US', {timeZone: 'Asia/Kolkata'})}
+            </p>
+            <p>Enjoy th show!! 🍿</p>
+            <p>Thanks for booking with us! <br/> - QuickShow Team - </p>
+            </div>`
+        })
+    }
+)
 
 
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation, releaseSeatsAndDeleteBooking];
+
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation, releaseSeatsAndDeleteBooking, sendBookingConformationEmail];
