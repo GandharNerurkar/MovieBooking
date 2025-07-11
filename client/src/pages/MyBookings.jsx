1ba82435cf20cab20e27fe496e05994ebb967d85
GandharNerurkar <gandhar.nerurkar@zapcg.com>
2025-07-11 17:00:58 +0530

    movie price converted use to inr

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -11,6 +11,8 @@ const MyBookings = () => {
   const currency = import.meta.env.VITE_CURRENCY;
   const [bookings, setBookings] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const USD_TO_INR = 83.5;
+
   const {
       axios,
       getToken,
@@ -64,7 +66,8 @@ const MyBookings = () => {
 
           <div className='flex flex-col md:items-end md:text-right justify-between p-4'>
             <div className='flex items-center gap-4'>
-              <p className='text-2xl font-semibold mb-3'>{currency}{item.amount}</p>
+              {/* <p className='text-2xl font-semibold mb-3'>{currency}{item.amount}</p> */}
+              <p className='text-2xl font-semibold mb-3'>₹{Math.round(item.amount * USD_TO_INR)}</p>
               {!item.isPaid && <Link to={item.paymentLink} className='bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer'>Pay Now</Link>}
             </div>
             <div className='text-sm'>
